b85cccc19cb49520e18cc0efb375f41129b7dc0f
Yusuf <abdullahyusufgungor@gmail.com>
2021-04-27 01:59:30 +0300

    2

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,27 @@
 function HomePage() {
-  return <div className="text-purple-500 bg-blue-100">Welcome to Next.js!</div>
+  return (
+    <>
+      <div className="site-container">
+        <div className="space-y-4">
+          <h1 className="text-2xl font-bold">Lorem ipsum dolor sit amet.</h1>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum
+            iusto labore obcaecati odit quod vero.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur,
+            velit?
+          </p>
+        </div>
+      </div>
+        <div className='max-w-4xl mx-auto mt-20'>
+            <img src="/i-am.jpg" alt=""/>
+        </div>
+        
+    </>
+  )
 }
 
 export default HomePage
- 
